a3245936ed22789632572dcd2a76729e7749331c
Daniel Derevjanik <daniel.derevjanik@gmail.com>
2026-04-10 21:01:04 +0200

    cicd: init

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/cattle-bity/' : '/',
   build: {
     rollupOptions: {
       input: {
